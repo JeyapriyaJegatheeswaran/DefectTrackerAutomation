5690be0df6fd174d13e506633abeaca3320f3561
Your Name <you@example.com>
2019-08-28 16:09:57 +0530

    Add priority

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -15,6 +15,11 @@ public class HomePage extends PageBase {
     private static String defectMenu = "//*[@id=\"root\"]/div/section/aside/div/ul/li[8]/div[1]";
     private static String defectSubMenu = "//*[@id='sub3$Menu']";
     private static String moduleMenu = "//a[@href=\"#/module\"]";
+    private static String settingMenu = "//*[@id=\"root\"]/div/section/aside/div/ul/li[9]/div";
+    private static String generalConfigSubMenu = "//*[@id=\"sub4$Menu\"]/li[4]/div";
+    private static String prioritySubmenu = "//*[@id=\"sub5$Menu\"]/li[1]/a";
+    private static String severitySubment = "//*[@id=\"sub5$Menu\"]/li[2]/a";
+
 
     public static void clickDefectMenu() {
         MethodBase.click_ByXpath(defectMenu);
@@ -27,7 +32,18 @@ public class HomePage extends PageBase {
     public static void clickModule(){
         MethodBase.click_ByXpath(moduleMenu);
     }
-
+    public static void clickSetting(){
+        MethodBase.click_ByXpath(settingMenu);
+    }
+    public static void clickGeneralconfig(){
+        MethodBase.click_ByXpath(generalConfigSubMenu);
+    }
+    public static void clickPriority(){
+        MethodBase.click_ByXpath(prioritySubmenu);
+    }
+    public static void clickSeverity(){
+        MethodBase.click_ByXpath(severitySubment);
+    }
 
 //    public static void Login(String username, String password) {
 //        MethodBase.setText_ByID(txtUserName,username);
--- src/main/java/pages/ModulePage.java
+++ src/main/java/pages/ModulePage.java
@@ -41,8 +41,4 @@ public class ModulePage {
         MethodBase.clickButton_ByXpath(btnAdd);
     }
 
-
-
-
-
 }
