0acacec62b81bd4a9460760ce2bb6770833943a4
Your Name <you@example.com>
2019-09-10 14:53:13 +0530

    severity edit

--- src/test/java/tests/Severity/AddSeverityTest.java
+++ src/test/java/tests/Severity/AddSeverityTest.java
@@ -5,13 +5,70 @@ import org.testng.asserts.SoftAssert;
 import pages.HomePage;
 
 import pages.SeverityPage;
+import utils.MethodBase;
 import utils.TestBase;
 
 import static utils.PageBase.implicitWait;
 
 public class AddSeverityTest extends TestBase {
 
-    @Test
+    @Test(priority = 1)
+    public void clickSettingVerify() {
+        implicitWait(5);
+        softAssert.assertEquals(HomePage.settingVerify(),"Setting");
+        HomePage.clickSetting();
+        softAssert.assertEquals(HomePage.generalConfigVerify(),"General Configuration");
+        HomePage.clickGeneralconfig();
+    }
+    @Test(priority = 2)
+    public void clickGeneranConfiSettingVerify() {
+        implicitWait(5);
+        HomePage.clickSetting();
+        HomePage.clickGeneralconfig();
+        softAssert.assertEquals(HomePage.severityVerify(),"Severity");
+        HomePage.clickSeverity();
+    }
+    @Test(priority = 3)
+    public void SeverityPageVerify() {
+        implicitWait(5);
+        HomePage.clickSetting();
+        HomePage.clickGeneralconfig();
+        HomePage.clickSeverity();
+        SeverityPage.isSeverityPageDisplay();
+    }
+
+    @Test(priority = 4)
+    public void SererityAddFormVerify() {
+        implicitWait(5);
+        HomePage.clickSetting();
+        HomePage.clickGeneralconfig();
+        HomePage.clickSeverity();
+        SeverityPage.isSeverityPageDisplay();
+        SeverityPage.clickAddSeveritybtn();
+        SeverityPage.isSeverityAddFormDisplay();
+        softAssert.assertEquals(SeverityPage.severityFormVerifyText(),"Add severity");
+
+    }
+
+    @Test(priority = 5)
+    public void formItemsVerify() {
+
+        implicitWait(5);
+        HomePage.clickSetting();
+        HomePage.clickGeneralconfig();
+        HomePage.clickSeverity();
+        SeverityPage.isSeverityPageDisplay();
+        SeverityPage.clickAddSeveritybtn();
+        softAssert.assertEquals(SeverityPage.isDisplayName(),"Name");
+        softAssert.assertEquals(SeverityPage.isDisplayDescription(),"Description");
+        softAssert.assertEquals(SeverityPage.isDisplayIcon(),"Icon");
+        softAssert.assertEquals(SeverityPage.isDisplayColor(),"Colour");
+        softAssert.assertEquals(SeverityPage.isDisplayOKBtn(),"OK");
+        softAssert.assertEquals(SeverityPage.isDisplayCancleBtn(),"Cancel");
+
+    }
+
+    @Test(priority = 6)
     public void addSeverity() {
 
         implicitWait(5);
@@ -30,7 +87,7 @@ public class AddSeverityTest extends TestBase {
         SeverityPage.clickAddOKbtn();
 
     }
-    @Test
+    @Test(priority = 8)
     public void addSeverity2() {
         implicitWait(5);
         HomePage.clickSetting();
@@ -48,7 +105,7 @@ public class AddSeverityTest extends TestBase {
         SeverityPage.clickAddOKbtn();
 
     }
-    @Test
+    @Test(priority = 9)
     public void addSeverity3() {
         implicitWait(5);
         HomePage.clickSetting();
