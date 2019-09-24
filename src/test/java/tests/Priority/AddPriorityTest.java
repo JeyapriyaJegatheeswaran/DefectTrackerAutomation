52454d1712cfabb1b4a7ee9d5124a6aee42fa91a
Your Name <you@example.com>
2019-09-24 15:12:51 +0530

    edit priority modified

--- src/test/java/tests/Priority/AddPriorityTest.java
+++ src/test/java/tests/Priority/AddPriorityTest.java
@@ -11,7 +11,7 @@ public class AddPriorityTest extends TestBase {
     private static final Logger LOGGER = Logger.getLogger(AddPriorityTest.class);
 
     @Test(priority = 101,groups = {"regression","test"})
-    public void clickSettingVerify() {
+    public void clickSetting() {
 
         implicitWait(5);
         softAssert.assertEquals(HomePage.settingVerify(),"Setting","Setting not verified");
@@ -23,14 +23,11 @@ public class AddPriorityTest extends TestBase {
         softAssert.assertEquals(HomePage.generalConfigVerify(),"General Configuration","General Configuration menu not displayed");
         LOGGER.info("Side Navigation_General Configuration button verified");
         extentTest.log(LogStatus.PASS,"Side Navigation_Setting button verified");
-        HomePage.clickGeneralconfig();
-        LOGGER.info("Side Navigation_General Configuration button clicked");
-        extentTest.log(LogStatus.PASS,"Side Navigation_General Configuration button clicked");
         softAssert.assertAll();
     }
 
     @Test(priority =102,groups = {"regression","test"})
-    public void clickGeneranConfiSettingVerify() {
+    public void clickGeneranConfiSetting() {
         implicitWait(5);
         HomePage.clickSetting();
         LOGGER.info("Side Navigation_Setting button clicked");
@@ -38,14 +35,26 @@ public class AddPriorityTest extends TestBase {
         HomePage.clickGeneralconfig();
         LOGGER.info("Side Navigation_General Configuration button clicked");
         extentTest.log(LogStatus.PASS,"Side Navigation_General Configuration button clicked");
-        softAssert.assertEquals(HomePage.priorityVerify(),"Priority","Priority not verified");
-        LOGGER.info("Side Navigation_Priority button verified");
-        extentTest.log(LogStatus.PASS,"Side Navigation_Priority button verified");
         softAssert.assertAll();
     }
-    @Test(priority = 103,groups = {"regression","test"})
-    public void Priority1PageVerify() {
 
+    @Test(priority = 103,groups = {"regression","test"})
+    public void submenuPriorityVerify() {
+        implicitWait(5);
+        HomePage.clickSetting();
+        LOGGER.info("Side Navigation_Setting button clicked");
+        extentTest.log(LogStatus.PASS, "Side Navigation_Setting button clicked");
+        HomePage.clickGeneralconfig();
+        LOGGER.info("Side Navigation_General Configuration button clicked");
+        extentTest.log(LogStatus.PASS, "Side Navigation_General Configuration button clicked");
+        HomePage.clickPriority();
+        LOGGER.info("Side Navigation_Priority button clicked");
+        extentTest.log(LogStatus.PASS, "Side Navigation_Priority button clicked");
+        softAssert.assertAll();
+    }
+
+    @Test(priority = 104,groups = {"regression","test"})
+    public void PriorityPageVerify() {
         implicitWait(5);
         HomePage.clickSetting();
         LOGGER.info("Side Navigation_Setting button clicked");
@@ -61,9 +70,9 @@ public class AddPriorityTest extends TestBase {
         extentTest.log(LogStatus.PASS,"Priority Page Displayed");
         softAssert.assertAll();
     }
-    @Test(priority = 104,groups = {"regression","test"})
-    public void addPriorityBtbVerify() {
 
+    @Test(priority = 105,groups = {"regression","test"})
+    public void PriorityAddButtonVerify() {
         implicitWait(5);
         HomePage.clickSetting();
         LOGGER.info("Side Navigation_Setting button clicked");
@@ -82,9 +91,127 @@ public class AddPriorityTest extends TestBase {
         extentTest.log(LogStatus.PASS,"Priority Page_Add button verified");
         softAssert.assertAll();
     }
-    @Test(priority = 105,groups = {"regression","test","smoke"})
-    public void addPriority1() {
 
+    @Test(priority = 106,groups = {"regression","test"})
+    public void PriorityAddFormVerify() {
+        implicitWait(5);
+        HomePage.clickSetting();
+        LOGGER.info("Side Navigation_Setting button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_Setting button clicked");
+        HomePage.clickGeneralconfig();
+        LOGGER.info("Side Navigation_General Configuration button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_General Configuration button clicked");
+        HomePage.clickPriority();
+        LOGGER.info("Side Navigation_Priority button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_Priority button clicked");
+        PriorityPage.isPriorityPageDisplay();
+        LOGGER.info("Priority Page Displayed");
+        extentTest.log(LogStatus.PASS,"Priority Page Displayed");
+        softAssert.assertEquals(PriorityPage.priorityAddButtonVerify(),"Add priority","Add priority button not verify");
+        LOGGER.info("Priority Page_Add button verified");
+        extentTest.log(LogStatus.PASS,"Priority Page_Add button verified");
+        PriorityPage.clickAddPrioritybtn();
+        LOGGER.info("Priority Page_Add button Clicked");
+        extentTest.log(LogStatus.PASS,"Priority Page_Add button Clicked");
+        staticWait(3);
+        softAssert.assertEquals(PriorityPage.priorityAddFormVerify(),"Add Priority","Add priority form not verify");
+        LOGGER.info("Priority Page_Add form verified");
+        extentTest.log(LogStatus.PASS,"Priority Page_Add form verified");
+        softAssert.assertAll();
+    }
+    @Test(priority = 107,groups = {"regression","test"})
+    public void formItemsVerify() {
+        implicitWait(5);
+        HomePage.clickSetting();
+        LOGGER.info("Side Navigation_Setting button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_Setting button clicked");
+        HomePage.clickGeneralconfig();
+        LOGGER.info("Side Navigation_General Configuration button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_General Configuration button clicked");
+        HomePage.clickPriority();
+        LOGGER.info("Side Navigation_Priority button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_Priority button clicked");
+        PriorityPage.isPriorityPageDisplay();
+        LOGGER.info("Priority Page Displayed");
+        extentTest.log(LogStatus.PASS,"Priority Page Displayed");
+        softAssert.assertEquals(PriorityPage.priorityAddButtonVerify(),"Add priority","Add priority button not verify");
+        LOGGER.info("Priority Page_Add button verified");
+        extentTest.log(LogStatus.PASS,"Priority Page_Add button verified");
+        PriorityPage.clickAddPrioritybtn();
+        LOGGER.info("Priority Page_Add button Clicked");
+        extentTest.log(LogStatus.PASS,"Priority Page_Add button Clicked");
+        staticWait(3);
+        softAssert.assertEquals(PriorityPage.isDisplayName(),"Name","Priority name not Displayed");
+        LOGGER.info("Priority name displayed");
+        extentTest.log(LogStatus.PASS,"Priority name displayed");
+        softAssert.assertEquals(PriorityPage.isDisplayDescription(),"Description","Priority Description not Displayed");
+        LOGGER.info("Priority description displayed");
+        extentTest.log(LogStatus.PASS,"Priority description displayed");
+        softAssert.assertEquals(PriorityPage.isDisplayIcon(),"Icon","Priority Icon not Displayed");
+        LOGGER.info("Priority Icon Displayed");
+        extentTest.log(LogStatus.PASS,"Priority Icon Displayed");
+        softAssert.assertEquals(PriorityPage.isDisplayColor(),"Colour","Priority color not Display");
+        LOGGER.info("Priority color Displayed");
+        extentTest.log(LogStatus.PASS,"Priority color Displayed");
+        softAssert.assertEquals(PriorityPage.isDisplayOKBtn(),"OK","OK button not Displayed");
+        LOGGER.info("OK button Displayed");
+        extentTest.log(LogStatus.PASS,"OK button Displayed");
+        softAssert.assertEquals(PriorityPage.isDisplayCancleBtn(),"Cancel","Cancel button not Displayed");
+        LOGGER.info("Cancel button Displayed");
+        extentTest.log(LogStatus.PASS,"Cancel button Displayed");
+        softAssert.assertAll();
+    }
+
+    @Test(priority = 108,groups = {"regression","test"})
+    public void cancelPriority() {
+        implicitWait(5);
+        HomePage.clickSetting();
+        LOGGER.info("Side Navigation_Setting button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_Setting button clicked");
+        HomePage.clickGeneralconfig();
+        LOGGER.info("Side Navigation_General Configuration button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_General Configuration button clicked");
+        HomePage.clickPriority();
+        LOGGER.info("Side Navigation_Priority button clicked");
+        extentTest.log(LogStatus.PASS,"Side Navigation_Priority button clicked");
+        PriorityPage.isPriorityPageDisplay();
+        LOGGER.info("Priority Page Displayed");
+        extentTest.log(LogStatus.PASS,"Priority Page Displayed");
+        PriorityPage.clickAddPrioritybtn();
+        LOGGER.info("Priority Page_Add button Clicked");
+        extentTest.log(LogStatus.PASS,"Priority Page_Add button Clicked");
+        PriorityPage.clearPeriorityName();
+        PriorityPage.setPeriorityName("High");
+        LOGGER.info("Priority Page_Name added");
+        extentTest.log(LogStatus.PASS,"Priority Page_Name added");
+        PriorityPage.clearPeriorityDescription();
+        PriorityPage.setPeriorityDescription("defect tracker configuration parts");
+        LOGGER.info("Priority Page_Description added");
+        extentTest.log(LogStatus.PASS,"Priority Page_Description added");
+        PriorityPage.setIcon();
+        LOGGER.info("Priority Page_Icon added");
+        extentTest.log(LogStatus.PASS,"Priority Page_Icon added");
+        PriorityPage.clickColor();
+        LOGGER.info("Priority Page_Colour Clicked");
+        extentTest.log(LogStatus.PASS,"Priority Page_Colour Clicked");
+        PriorityPage.selectColor();
+        LOGGER.info("Priority Page_Colour Selected");
+        extentTest.log(LogStatus.PASS,"Priority Page_Colour Selected");
+        softAssert.assertEquals(PriorityPage.isDisplayCancleBtn(),"Cancel","Cancel button not Displayed");
+        LOGGER.info("Cancle button Displayed");
+        extentTest.log(LogStatus.PASS,"Cancle button Displayed");
+        PriorityPage.clickCancelBtn();
+        LOGGER.info("Cancel button Clicked");
+        extentTest.log(LogStatus.PASS,"Cancel button Clicked");
+        staticWait(3);
+        softAssert.assertEquals(PriorityPage.isPriorityPageDisplay(),"Priority Configuration","Priority page is not displayed");
+        LOGGER.info("Priority Page Displayed");
+        extentTest.log(LogStatus.PASS,"Priority Page Displayed");
+        softAssert.assertAll();
+    }
+
+    @Test(priority = 109,groups = {"regression","test","smoke"})
+    public void addPriority1() {
         implicitWait(5);
         HomePage.clickSetting();
         LOGGER.info("Side Navigation_Setting button clicked");
@@ -125,7 +252,7 @@ public class AddPriorityTest extends TestBase {
 
     }
 
-    @Test(priority = 106,groups = {"regression","test","smoke"})
+    @Test(priority = 110,groups = {"regression","test","smoke"})
     public void addPriority2() {
 
         implicitWait(5);
@@ -168,7 +295,7 @@ public class AddPriorityTest extends TestBase {
 
     }
 
-    @Test(priority = 107,groups = {"regression","test","smoke"})
+    @Test(priority = 111,groups = {"regression","test","smoke"})
     public void addPriority3() {
 
         implicitWait(5);
@@ -209,7 +336,7 @@ public class AddPriorityTest extends TestBase {
         extentTest.log(LogStatus.PASS,"Priority Page_OK button clicked");
         softAssert.assertAll();
     }
-    @Test(priority = 108,groups = {"regression","test","smoke"})
+    @Test(priority = 112,groups = {"regression","test","smoke"})
     public void addPriority4() {
 
         implicitWait(5);
